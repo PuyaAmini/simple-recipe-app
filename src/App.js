1d929068e56d03aebf43fea732bc396243094c83
pamini <puyagit@gmail.com>
2024-04-28 20:23:39 +0330

    Router & Pages Setup [s11 e2] v80

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+
+// pages
+import Home from './pages/home/Home'
+import Create from './pages/create/Create'
+import Search from './pages/search/Search'
+import Recipe from './pages/recipe/Recipe'
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/create' element={<Create/>}/>
+        <Route path='/search' element={<Search/>}/>
+        <Route path='/recipe' element={<Recipe/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/create/Create.js
@@ -0,0 +1,12 @@
+// styles
+import './Create.css'
+
+import React from 'react'
+
+export default function create() {
+  return (
+    <div>
+      Create
+    </div>
+  )
+}
