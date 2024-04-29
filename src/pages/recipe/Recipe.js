3c3207e549fbd83ff4abff8d368e23fe47fd0446
pamini <puyagit@gmail.com>
2024-04-29 19:15:35 +0330

    Fetching a Single Recipe in Recipe each page by useFetch + useParams

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,12 +1,20 @@
 // styles
-import './Recipe.css'
+import "./Recipe.css";
 
-import React from 'react'
+import React from "react";
+import useFetch from "../../hooks/useFetch";
+import { useParams } from "react-router-dom";
 
 export default function Recipe() {
+  const { id } = useParams();
+  // {id} = in App.js    <Route path='/recipe/:id' element={<Recipe/>}/>
+  const url = "http://localhost:3000/recipes/" + id;
+  const { data: recipe, isPending, error } = useFetch(url);
   return (
-    <div>
-      Recipes
+    <div className="recipe">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Caricamento...</p>}
+      {recipe && <h1>{recipe.title}</h1>}
     </div>
-  )
+  );
 }
