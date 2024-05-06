2415268d224bb877b78a4c03d4a07d44be7a2b32
pamini <puyagit@gmail.com>
2024-05-06 18:06:55 +0330

    Color Theme Selector added [s12 e7] git99

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,11 +11,11 @@ import { useTheme } from '../hooks/useTheme'
 
 export default function Navbar() {
 
-  const {color , changeColor} = useTheme()
+  const {color } = useTheme()
 
   return (
     <div className='navbar' style={{background:color}}>
-      <nav onClick={() => changeColor('pink')}>
+      <nav>
        <Link to={'/'} className='brand'><h1>Ninja Ricetta</h1></Link>
        <Searchbar/>
        <Link to={'/create'}>Create Recipe</Link>
