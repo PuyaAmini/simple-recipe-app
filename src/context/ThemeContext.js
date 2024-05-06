26f846160aba8185cbe4989fb272fb0008661465
pamini <puyagit@gmail.com>
2024-05-06 19:23:12 +0330

    Light/Dark mode selector #added (part 1) [s12 e8] git100

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -2,24 +2,32 @@ import { createContext, useReducer } from "react";
 
 export const ThemeContext = createContext();
 
-const themeReducer = (state , action) =>{
-       switch(action.type){
-              case 'CHANGE_COLOR':
-                     return {...state , color: action.payload}
-       }
-}
+const themeReducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE_COLOR":
+      return { ...state, color: action.payload };
+    case "CHANGE_MODE":
+      return { ...state, mode: action.payload };
+    default:
+      return state;
+  }
+};
 
 export function ThemeProvider({ children }) {
-       const[ state , dispatch] = useReducer(themeReducer ,{
-              color:'#491b86'
-       })
+  const [state, dispatch] = useReducer(themeReducer, {
+    color: "#491b86",
+    mode: "dark",
+  });
 
-       const changeColor = (color) =>{
-              dispatch({type: 'CHANGE_COLOR' , payload: color}) //({action.type , action.payload})
-       }
+  const changeColor = (color) => {
+    dispatch({ type: "CHANGE_COLOR", payload: color }); //({action.type , action.payload})
+  };
+  const changeMode = (mode) => {
+    dispatch({ type: "CHANGE_MODE", payload: mode });
+  };
 
   return (
-    <ThemeContext.Provider value={{ ...state , changeColor }}>
+    <ThemeContext.Provider value={{ ...state, changeColor, changeMode }}>
       {children}
     </ThemeContext.Provider>
   );
