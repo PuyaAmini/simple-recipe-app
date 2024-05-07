d2e882becc1140916dbe7dc871bf78829aa70e30
pamini <puyagit@gmail.com>
2024-05-07 18:45:14 +0330

    Connecting to Firebase [s13 e4] git104

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/compat/app"; //core
+import "firebase/firestore"; //import any other fetcher we want
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCH1z107ZXRdKq3OXTk2J6XnvcxqlEg2Q4",
+  authDomain: "simple-recipe-app-80ed4.firebaseapp.com",
+  projectId: "simple-recipe-app-80ed4",
+  storageBucket: "simple-recipe-app-80ed4.appspot.com",
+  messagingSenderId: "256647110711",
+  appId: "1:256647110711:web:eea3d1b0ce812da4229fb2",
+};
+
+//init firebase
+
+firebase.initializeApp(firebaseConfig) //conect to our firebase backend
+
+//init services
+const projectFirestore = firebase.firestore() 
+
+export {projectFirestore}
