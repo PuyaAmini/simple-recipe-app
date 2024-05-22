ea85391c7c96f7c4f57effaa2a792df7488bc7ce
pamini <puyagit@gmail.com>
2024-05-22 12:19:47 +0330

    fetching documents from fireStore [s13 e5] git-106

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { projectFirestore } from "../../firebase/config";
+
 // styles
 import "./Recipe.css";
-import { useFetch } from "../../hooks/useFetch";
 import { useTheme } from "../../hooks/useTheme";
 
 export default function Recipe() {
   const { id } = useParams();
-  const url = "http://localhost:3000/recipes/" + id;
-  const { data: recipe, error, isPending } = useFetch(url);
+
+  const [recipe, setRecipe] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(false);
+
   const { mode } = useTheme();
+
+  useEffect(() => {
+    setIsPending(true);
+
+    projectFirestore
+      .collection("recipes")
+      .doc(id)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          setIsPending(false);
+          setRecipe(doc.data());
+        } else {
+          setIsPending(false);
+          setError("could not find recipe..");
+        }
+      });
+  }, [id]); 
+  //the "id" will not change but we have to add it for the sake of warnings
+
   return (
     <div className={`recipe ${mode}`}>
       {error && <p className="error">{error}</p>}
