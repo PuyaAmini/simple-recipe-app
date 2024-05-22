ec3fc62b4431bb531d2f36247bf4f3d65417cb8c
pamini <puyagit@gmail.com>
2024-05-22 13:51:58 +0330

    delete doc from firebase firestore dataset [s13 e7] git-108

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,12 +1,22 @@
 import React from "react";
-import {useTheme} from '../hooks/useTheme'
+import { useTheme } from "../hooks/useTheme";
+import Trashcan from "../assets/Trashcan.svg";
+import { projectFirestore } from "../firebase/config";
 
 // styles:
 import "./RecipeList.css";
 import { Link } from "react-router-dom";
 
+const handleClick = async (id) => {
+  try {
+    await projectFirestore.collection("recipes").doc(id).delete();
+  } catch (err) {
+    console.log(err)
+  }
+};
+
 export default function RecipeList({ recipes }) {
-  const {mode} = useTheme()
+  const { mode } = useTheme();
   return (
     <div className="recipe-list">
       {recipes.map((recipe) => (
@@ -15,6 +25,12 @@ export default function RecipeList({ recipes }) {
           <p>{recipe.cookingTime} to make.</p>
           <snap>{recipe.method.substring(0, 100)}...</snap>
           <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
+          <img
+            src={Trashcan}
+            alt="remove"
+            className="delete"
+            onClick={() => handleClick(recipe.id)}
+          />
         </div>
       ))}
     </div>
